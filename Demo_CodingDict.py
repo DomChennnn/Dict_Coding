93dc37c4ac28e563410104d1498048634ac9ff95
Domchennnn <1345656499@qq.com>
2022-08-05 14:13:58 +0800

    second commit

--- Demo_CodingDict.py
+++ Demo_CodingDict.py
@@ -14,7 +14,106 @@ from uniquant import uniquant
 from myreshape import myreshape
 from mypred import mypred
 
+
+# test on one channel
+# # init
+# encode_start = time.time()
+# foldername = 'GenerallImages'
+# blk_size = 8
+# transform = 'm79'
+#
+# path_in = './/Images//' + foldername + '//Test2//'                      # Image dir path
+# data = scio.loadmat('.//Dictionary//Dict_RLS_' + foldername + '.mat')   # load Dictionary path
+# D = data['dlsRLS'][0, 0][0]                                             # Dictionary
+#
+# N, K = D.shape
+# L = 40000
+# Ds = Dictionarys(D, K, N, L, transform)                                 #define Dictionary
+# Dimg = glob.glob(path_in + '//**//*.bmp', recursive=True)               # Image path
+# NumberImages = len(Dimg)
+#
+# Bitrate_JPEG = np.transpose(
+#     h5py.File('.//Results//Bitrate_JPEG_' + foldername + '.mat', 'r')['Bitrate_JPEG'])  # load bitrate
+# Quality_JPEG = np.transpose(
+#     h5py.File('.//Results//Quality_JPEG_' + foldername + '.mat', 'r')['Quality_JPEG'])  # load quality
+#
+# numBit = Bitrate_JPEG.shape[1]
+#
+# Bitrate_Dict = np.zeros((NumberImages, numBit))
+# Quality_Dict = np.zeros((NumberImages, numBit))
+#
+# #encode, this can be divided into two steps, including dictionary encoding and res encoding
+# #dictionary encode
+# for ind_img in range(NumberImages):
+#     Quality_Dict[ind_img, :], Bitrate_Dict[ind_img,:], res, xc, dele, deldc, thr, thrdc, xc_encoded, res_encoded = test_Dict(Dimg[ind_img], Bitrate_JPEG[[ind_img], :], Ds)
+#
+# for i in range(8):
+#     xc[i] = list(xc[i])
+#
+# xc_encoded = bz2.compress((str(xc)).encode(), 9)
+#
+# #dictionary encode
+# h, w = res.shape
+# res = res.reshape((-1, 1))
+# res_list = []
+# for i in range(len(res)):
+#     res_list.append(res[i, 0])
+#
+# res_encoded = bz2.compress((str(res_list)).encode(), 9)
+# print(len(xc_encoded))
+# print((len(res_encoded) + len(xc_encoded)))
+# encode_end = time.time()
+# print('encode time:')
+# print((encode_end-encode_start)*1000)
+#
+# #decode
+# start_time = time.time()
+#
+# #res decode
+# res_decode = (bz2.decompress(res_encoded)).decode()
+# res = eval(res_decode)
+# res = np.array(res)
+# res = res.reshape((h, w))
+#
+# #dictionary decode
+# xc_decode = (bz2.decompress(xc_encoded)).decode()
+# xc_decode = xc_decode.split('], [')
+# xc_decode[0] = xc_decode[0][2:]
+# xc_decode[len(xc_decode) - 1] = xc_decode[len(xc_decode) - 1][0:-2]
+# for i in range(8):
+#     if xc_decode[i] == '':
+#         xc_decode[i] = []
+#     else:
+#         xc_decode[i] = list(eval(xc_decode[i]))
+# xc = xc_decode
+#
+# xCw = xc[0:4]
+# xCdc = xc[4:]
+# Zw_r = myreshape(xCw)
+# Zdc_r = mypred(xCdc)
+# Zdc_r = (Zdc_r.T).reshape(1, 988)
+#
+# Qdc = uniquant(Zdc_r, deldc, thrdc)             # inverse quantizing
+# Qw = uniquant(Zw_r, dele, thr)                  # inverse quantizing
+# dcnz = np.sum(Zdc_r != 0)                       # number of non-zeros in DC
+# S = np.sum(Zw_r != 0)                           # selected number of non-zeros for each column
+# sumS = np.sum(S)
+#
+# Xr = np.concatenate((Qdc, np.zeros((N - 1, 988))), axis=0)
+# Xa = np.dot(np.float64(Ds.D), Qw)
+# Ar = mycol2im(Xr + Xa, transform='m79', imsize=[304, 208], size=[8, 8])
+# Ar = Ar[0: 301, 0: 201]
+#
+# pic = Image.fromarray(np.uint8(Ar + res + 128))
+# pic.save('test.png')
+# end_time = time.time()
+# print('finish, and the img is saved as test.png')
+# print('decoding time:')
+# print((end_time - start_time) * 1000)
+
+# test on RGB
 # init
+encode_start = time.time()
 foldername = 'GenerallImages'
 blk_size = 8
 transform = 'm79'
@@ -42,67 +141,111 @@ Quality_Dict = np.zeros((NumberImages, numBit))
 #encode, this can be divided into two steps, including dictionary encoding and res encoding
 #dictionary encode
 for ind_img in range(NumberImages):
-    Quality_Dict[ind_img, :], Bitrate_Dict[ind_img,:], res, xc, dele, deldc, thr, thrdc, xc_encoded, res_encoded = test_Dict(Dimg[ind_img], Bitrate_JPEG[[ind_img], :], Ds)
+    img = Image.open(Dimg[ind_img]).convert('RGB')
+    img = np.array(img, dtype=np.double)
+    Quality_Dict[ind_img, :], Bitrate_Dict[ind_img,:], res_R, xc_R, dele_R, deldc_R, thr_R, thrdc_R, _, _ = test_Dict(img[:,:,0], Bitrate_JPEG[[ind_img], :], Ds)
+    Quality_Dict[ind_img, :], Bitrate_Dict[ind_img,:], res_G, xc_G, dele_G, deldc_G, thr_G, thrdc_G, _, _ = test_Dict(img[:,:,1], Bitrate_JPEG[[ind_img], :], Ds)
+    Quality_Dict[ind_img, :], Bitrate_Dict[ind_img,:], res_B, xc_B, dele_B, deldc_B, thr_B, thrdc_B, _, _ = test_Dict(img[:,:,2], Bitrate_JPEG[[ind_img], :], Ds)
 
 for i in range(8):
-    xc[i] = list(xc[i])
+    xc_R[i] = list(xc_R[i])
+    xc_G[i] = list(xc_G[i])
+    xc_B[i] = list(xc_B[i])
 
-xc_encoded = bz2.compress((str(xc)).encode(), 9)
+xc_encoded_R = bz2.compress((str(xc_R)).encode(), 9)
+xc_encoded_G = bz2.compress((str(xc_G)).encode(), 9)
+xc_encoded_B = bz2.compress((str(xc_B)).encode(), 9)
 
 #dictionary encode
-h, w = res.shape
-res = res.reshape((-1, 1))
-res_list = []
-for i in range(len(res)):
-    res_list.append(res[i, 0])
+h, w = res_R.shape
 
-res_encoded = bz2.compress((str(res_list)).encode(), 9)
+res_R = res_R.reshape((-1, 1))
+res_G = res_G.reshape((-1, 1))
+res_B = res_B.reshape((-1, 1))
+res_list_R = []
+res_list_G = []
+res_list_B = []
+for i in range(len(res_R)):
+    res_list_R.append(res_R[i, 0])
+    res_list_G.append(res_G[i, 0])
+    res_list_B.append(res_B[i, 0])
 
-# print((len(res_encoded) + len(xc_encoded)))
+res_encoded_R = bz2.compress((str(res_list_R)).encode(), 9)
+res_encoded_G = bz2.compress((str(res_list_G)).encode(), 9)
+res_encoded_B = bz2.compress((str(res_list_B)).encode(), 9)
+
+print(len(xc_encoded_R)+len(xc_encoded_G)+len(xc_encoded_B))
+print(len(xc_encoded_R)+len(xc_encoded_G)+len(xc_encoded_B)+len(res_encoded_R)+len(res_encoded_G)+len(res_encoded_B))
+encode_end = time.time()
+print('encode time:')
+print((encode_end-encode_start)*1000)
 
 #decode
 start_time = time.time()
 
 #res decode
-res_decode = (bz2.decompress(res_encoded)).decode()
-res = eval(res_decode)
-res = np.array(res)
-res = res.reshape((h, w))
+res_decode_R = (bz2.decompress(res_encoded_R)).decode()
+res_decode_G = (bz2.decompress(res_encoded_G)).decode()
+res_decode_B = (bz2.decompress(res_encoded_B)).decode()
+res_R = eval(res_decode_R)
+res_G = eval(res_decode_G)
+res_B = eval(res_decode_B)
+res_R = np.array(res_R)
+res_G = np.array(res_G)
+res_B = np.array(res_B)
+res_R = res_R.reshape((h, w))
+res_G = res_G.reshape((h, w))
+res_B = res_B.reshape((h, w))
 
 #dictionary decode
-xc_decode = (bz2.decompress(xc_encoded)).decode()
-xc_decode = xc_decode.split('], [')
-xc_decode[0] = xc_decode[0][2:]
-xc_decode[len(xc_decode) - 1] = xc_decode[len(xc_decode) - 1][0:-2]
-for i in range(8):
-    if xc_decode[i] == '':
-        xc_decode[i] = []
-    else:
-        xc_decode[i] = list(eval(xc_decode[i]))
-xc = xc_decode
+xc_encoded = [xc_encoded_R,xc_encoded_G,xc_encoded_B]
+deldc = [deldc_R,deldc_G,deldc_B]
+thrdc = [thrdc_R,thrdc_G,thrdc_B]
+dele = [dele_R,dele_G,dele_B]
+thr = [thr_R,thr_G,thr_B]
 
-xCw = xc[0:4]
-xCdc = xc[4:]
-Zw_r = myreshape(xCw)
-Zdc_r = mypred(xCdc)
-Zdc_r = (Zdc_r.T).reshape(1, 988)
+for idx_channel in range(3):
+    xc_decode = (bz2.decompress(xc_encoded[idx_channel])).decode()
+    xc_decode = xc_decode.split('], [')
+    xc_decode[0] = xc_decode[0][2:]
+    xc_decode[len(xc_decode) - 1] = xc_decode[len(xc_decode) - 1][0:-2]
+    for i in range(8):
+        if xc_decode[i] == '':
+            xc_decode[i] = []
+        else:
+            xc_decode[i] = list(eval(xc_decode[i]))
+    xc = xc_decode
 
-Qdc = uniquant(Zdc_r, deldc, thrdc)             # inverse quantizing
-Qw = uniquant(Zw_r, dele, thr)                  # inverse quantizing
-dcnz = np.sum(Zdc_r != 0)                       # number of non-zeros in DC
-S = np.sum(Zw_r != 0)                           # selected number of non-zeros for each column
-sumS = np.sum(S)
+    xCw = xc[0:4]
+    xCdc = xc[4:]
+    Zw_r = myreshape(xCw)
+    Zdc_r = mypred(xCdc)
+    Zdc_r = (Zdc_r.T).reshape(1, -1)
 
-Xr = np.concatenate((Qdc, np.zeros((N - 1, 988))), axis=0)
-Xa = np.dot(np.float64(Ds.D), Qw)
-Ar = mycol2im(Xr + Xa, transform='m79', imsize=[304, 208], size=[8, 8])
-Ar = Ar[0: 301, 0: 201]
+    Qdc = uniquant(Zdc_r, deldc[idx_channel], thrdc[idx_channel])             # inverse quantizing
+    Qw = uniquant(Zw_r, dele[idx_channel], thr[idx_channel])                  # inverse quantizing
+    dcnz = np.sum(Zdc_r != 0)                       # number of non-zeros in DC
+    S = np.sum(Zw_r != 0)                           # selected number of non-zeros for each column
+    sumS = np.sum(S)
 
+    Xr = np.concatenate((Qdc, np.zeros((N - 1, 1024))), axis=0)
+    Xa = np.dot(np.float64(Ds.D), Qw)
+    Ar = mycol2im(Xr + Xa, transform='m79', imsize=[256, 256], size=[8, 8])
+    # Ar = Ar[0: 301, 0: 201]
+    if idx_channel == 0:
+        Ar_R = Ar
+    if idx_channel == 1:
+        Ar_G = Ar
+    if idx_channel == 2:
+        Ar_B = Ar
+
+res = np.stack((res_R,res_G,res_B),2)
+Ar = np.stack((Ar_R,Ar_G,Ar_B),2)
 pic = Image.fromarray(np.uint8(Ar + res + 128))
-pic.save('test.png')
+pic.save('test_no_loss.png')
 end_time = time.time()
+pic = Image.fromarray(np.uint8(Ar + 128))
+pic.save('test_with_loss.png')
 print('finish, and the img is saved as test.png')
 print('decoding time:')
 print((end_time - start_time) * 1000)
-
-
--- test_Dict.py
+++ test_Dict.py
@@ -6,89 +6,91 @@ from utils import test_Dict_par
 from imageapprox import imageapprox
 
 #one channel test
-def test_Dict(filename, Bitrate_JPEG, Ds):
+def test_Dict(img, Bitrate_JPEG, Ds):
     #init
-    img = Image.open(filename)
+    # img = Image.open(filename)
     img = np.array(img,dtype=np.double)
     img_hgt, img_wdt = img.shape
     numBits = Bitrate_JPEG.shape[1]
     psnr_val = np.zeros((1, numBits))
     bpp_val = np.zeros((1,numBits))
-    cur_QP = 80
+    cur_QP = 50
     im_128 = img - 128
 
-    for num in range(numBits):
-        if num > 8:
-            break
-        bestQP = cur_QP
-        target_bpp = Bitrate_JPEG[0, num]
-        par = test_Dict_par(dictionary = Ds, targetPSNR = 100 - cur_QP, dele = -18, qLimit = [0.4, 1.00], estimateBits = 'Huff06', ompMethod = 'mexOMP', verbose = 0)
-        #test_Dict_par is a dict in python
-        Ar, PSNR, xC, dele, deldc, thr, thrdc = imageapprox(im_128, par) #the main processing function
+    # for num in range(numBits):
+    #     if num > 8:
+    #         break
+    bestQP = cur_QP
+    target_bpp = 0.2
+    # target_bpp = Bitrate_JPEG[0, num]
+    par = test_Dict_par(dictionary = Ds, targetPSNR = 100 - cur_QP, dele = -18, qLimit = [0.4, 1.00], estimateBits = 'Huff06', ompMethod = 'mexOMP', verbose = 0)
+    #test_Dict_par is a dict in python
+    Ar, PSNR, xC, dele, deldc, thr, thrdc = imageapprox(im_128, par) #the main processing function
 
-        xc_encoded = bz2.compress((str(xC)).encode())
-        bits = len(xc_encoded)*8
+    xc_encoded = bz2.compress((str(xC)).encode())
+    bits = len(xc_encoded)*8
 
-        bestPSNR = PSNR
-        bpp_val_each = bits/(img_hgt*img_wdt)
-        cur_bpp = bpp_val_each
+    bestPSNR = PSNR
+    bpp_val_each = bits/(img_hgt*img_wdt)
+    cur_bpp = bpp_val_each
 
+    if cur_bpp > target_bpp:
+        dir1 = -1
+    else:
+        dir1 = 1
+
+    dirchange = 0
+
+    #search for better results
+    while bestPSNR<30:
         if cur_bpp > target_bpp:
-            dir1 = -1
+            cur_QP = cur_QP + 1
+            if dir1==-1:
+                dirchange = 0
+            else:
+                dirchange = 1
+
         else:
-            dir1 = 1
-
-        dirchange = 0
-
-        #search for better results
-        while (abs(target_bpp - cur_bpp)>0.05) and bestPSNR<30:
-            if cur_bpp > target_bpp:
-                cur_QP = cur_QP + 1
-                if dir1==-1:
-                    dirchange = 0
-                else:
-                    dirchange = 1
-
+            cur_QP = cur_QP - 1
+            if dir1==1:
+                dirchange = 0
             else:
-                cur_QP = cur_QP - 1
-                if dir1==1:
-                    dirchange = 0
-                else:
-                    dirchange = 1
+                dirchange = 1
 
-            if dirchange==1 or cur_QP>99 or cur_QP<1:
-                break
+        if dirchange==1 or cur_QP>99 or cur_QP<1:
+            break
 
-            par = test_Dict_par(dictionary=Ds, targetPSNR=100 - cur_QP, dele=-18, qLimit=[0.4, 1.00],
-                                estimateBits='Huff06', ompMethod='mexOMP', verbose=0)
-            Ar_now, PSNR_now, xC_now, dele, deldc, thr, thrdc = imageapprox(im_128, par)
+        par = test_Dict_par(dictionary=Ds, targetPSNR=100 - cur_QP, dele=-18, qLimit=[0.4, 1.00],
+                            estimateBits='Huff06', ompMethod='mexOMP', verbose=0)
+        Ar_now, PSNR_now, xC_now, dele, deldc, thr, thrdc = imageapprox(im_128, par)
 
 
-            if bestPSNR>PSNR_now:
-                break
-            else:
-                Ar = Ar_now
-                xC = xC_now
-                xc_encoded = bz2.compress((str(xC)).encode())
-                bits = len(xc_encoded) * 8
-                bestPSNR = PSNR_now
-                bpp_val_each = (bits) / (img_hgt * img_wdt)
-                cur_bpp = bpp_val_each
-                dele_fin, deldc_fin, thr_fin, thrdc_fin = dele, deldc, thr, thrdc
+        if bestPSNR>PSNR_now:
+            break
+        else:
+            Ar = Ar_now
+            xC = xC_now
+            xc_encoded = bz2.compress((str(xC)).encode())
+            bits = len(xc_encoded) * 8
+            bestPSNR = PSNR_now
+            bpp_val_each = (bits) / (img_hgt * img_wdt)
+            cur_bpp = bpp_val_each
 
-        psnr_val[0,num] = bestPSNR
-        bpp_val[0,num] = bpp_val_each
+    dele_fin, deldc_fin, thr_fin, thrdc_fin = dele, deldc, thr, thrdc
+    psnr_val = bestPSNR
+    bpp_val = bpp_val_each
 
     res_Ar = np.uint8(im_128 - Ar)
     res_encoded = bz2.compress((str(res_Ar)).encode())
 
-    ori = im_128.reshape((-1,1))
-    ori_list = []
-    for i in range(len(ori)):
-        ori_list.append(ori[i, 0])
-    ori_encoded = bz2.compress((str(ori_list)).encode())
-    ori_bits = len(ori_encoded)
-    print(ori_bits)
+    print('bestPSNR:')
+    print(bestPSNR)
+    # ori = im_128.reshape((-1,1))
+    # ori_list = []
+    # for i in range(len(ori)):
+    #     ori_list.append(ori[i, 0])
+    # ori_encoded = bz2.compress((str(ori_list)).encode())
+    # ori_bits = len(ori_encoded)
 
     # save img
     # img_Ar = Image.fromarray(np.uint8(Ar+res_Ar+128))
@@ -99,7 +101,7 @@ def test_Dict(filename, Bitrate_JPEG, Ds):
 
 
 
-# #RGB
+# # RGB
 # def test_Dict(filename, Bitrate_JPEG, Ds):
 #     img = Image.open(filename).convert('RGB')
 #     img = np.array(img,dtype=np.double)
@@ -107,92 +109,89 @@ def test_Dict(filename, Bitrate_JPEG, Ds):
 #     numBits = Bitrate_JPEG.shape[1]
 #     psnr_val = np.zeros((1, numBits))
 #     bpp_val = np.zeros((1,numBits))
-#     cur_QP = 80
+#     cur_QP = 65
 #     im_128 = img - 128
 #
-#     for num in range(numBits):
-#         # print(num)
-#         if num > 8:
+#     # for num in range(numBits):
+#     #     # print(num)
+#     #     if num > 8:
+#     #         break
+#
+#     bestQP = cur_QP
+#     target_bpp = 0.2
+#     par = test_Dict_par(dictionary = Ds, targetPSNR = 100 - cur_QP, dele = -18, qLimit = [0.4, 1.00], estimateBits = 'Huff06', ompMethod = 'mexOMP', verbose = 0)
+#     for i in range(3):
+#         Ar, PSNR, xC, dele, deldc, thr, thrdc = imageapprox(im_128[:,:,i], par)
+#
+#         xc_encoded = bz2.compress((str(xC)).encode())
+#         bits = len(xc_encoded)*8
+#
+#         bestPSNR = PSNR
+#         bpp_val_each = bits/(img_hgt*img_wdt)
+#         cur_bpp = bpp_val_each
+#
+#         if i == 0:
+#             R = Ar
+#             bits_R, dele_R, deldc_R, thr_R, thrdc_R = bits, dele, deldc, thr, thrdc
+#         elif i == 1:
+#             G = Ar
+#             bits_G, dele_G, deldc_G, thr_G, thrdc_G = bits, dele, deldc, thr, thrdc
+#         else:
+#             B = Ar
+#             bits_B, dele_B, deldc_B, thr_B, thrdc_B = bits, dele, deldc, thr, thrdc
+#
+#     if cur_bpp > target_bpp:
+#         dir1 = -1
+#     else:
+#         dir1 = 1
+#
+#     dirchange = 0
+#     while bestPSNR<30:
+#         if cur_bpp > target_bpp:
+#             cur_QP = cur_QP + 1
+#             if dir1==-1:
+#                 dirchange = 0
+#             else:
+#                 dirchange = 1
+#
+#         else:
+#             cur_QP = cur_QP - 1
+#             if dir1==1:
+#                 dirchange = 0
+#             else:
+#                 dirchange = 1
+#
+#         if dirchange==1 or cur_QP>99 or cur_QP<1:
 #             break
 #
-#         bestQP = cur_QP
-#         target_bpp = Bitrate_JPEG[0, num]
-#         par = test_Dict_par(dictionary = Ds, targetPSNR = 100 - cur_QP, dele = -18, qLimit = [0.4, 1.00], estimateBits = 'Huff06', ompMethod = 'mexOMP', verbose = 0)
+#         par = test_Dict_par(dictionary=Ds, targetPSNR=100 - cur_QP, dele=-18, qLimit=[0.4, 1.00],
+#                             estimateBits='Huff06', ompMethod='mexOMP', verbose=0)
 #         for i in range(3):
-#             Ar, PSNR, xC = imageapprox(im_128[:,:,i], par)
+#             Ar_now, PSNR_now, xC_now, dele, deldc, thr, thrdc = imageapprox(im_128[:,:,i], par)
 #
-#             xc_encoded = bz2.compress((str(xC)).encode())
-#             bits = len(xc_encoded)*8
-#
-#             bestPSNR = PSNR
-#             bpp_val_each = bits/(img_hgt*img_wdt)
-#             cur_bpp = bpp_val_each
-#
-#             if i == 0:
-#                 R = Ar
-#             elif i == 1:
-#                 G = Ar
+#             if bestPSNR>PSNR_now:
+#                 continue
 #             else:
-#                 B = Ar
-#
-#         if cur_bpp > target_bpp:
-#             dir1 = -1
-#         else:
-#             dir1 = 1
-#
-#         dirchange = 0
-#         while (abs(target_bpp - cur_bpp)>0.05) and bestPSNR<40:
-#         # while (target_bpp>cur_bpp) or bestPSNR<40:
-#             if cur_bpp > target_bpp:
-#                 cur_QP = cur_QP + 1
-#                 if dir1==-1:
-#                     dirchange = 0
-#                 else:
-#                     dirchange = 1
-#
-#             else:
-#                 cur_QP = cur_QP - 1
-#                 if dir1==1:
-#                     dirchange = 0
-#                 else:
-#                     dirchange = 1
-#
-#             if dirchange==1 or cur_QP>99 or cur_QP<1:
-#                 break
-#
-#             par = test_Dict_par(dictionary=Ds, targetPSNR=100 - cur_QP, dele=-18, qLimit=[0.4, 1.00],
-#                                 estimateBits='Huff06', ompMethod='mexOMP', verbose=0)
-#             for i in range(3):
-#                 Ar_now, PSNR_now, xC_now = imageapprox(im_128[:,:,i], par)
-#
-#                 if bestPSNR>PSNR_now:
-#                     # xc_encoded = bz2.compress((str(xC)).encode())
-#                     # bits = len(xc_encoded)*8
-#                     # bpp_val_each = (bits) / (img_hgt * img_wdt)
-#                     # cur_bpp = bpp_val_each
-#                     continue
-#                 else:
-#                     Ar = Ar_now
-#                     xC = xC_now
-#                     xc_encoded = bz2.compress((str(xC)).encode())
-#                     bits = len(xc_encoded) * 8
-#                     bestPSNR = PSNR_now
-#                     bpp_val_each = (bits) / (img_hgt * img_wdt)
-#                     cur_bpp = bpp_val_each
+#                 xC = xC_now
+#                 xc_encoded = bz2.compress((str(xC)).encode())
+#                 bits = len(xc_encoded) * 8
+#                 bestPSNR = PSNR_now
+#                 bpp_val_each = (bits) / (img_hgt * img_wdt)
+#                 cur_bpp = bpp_val_each
 #
 #                 if i == 0:
-#                     R = Ar
-#                     bits_R = bits
+#                     R = Ar_now
+#                     bits_R, dele_R, deldc_R, thr_R, thrdc_R = bits, dele, deldc, thr, thrdc
 #                 elif i == 1:
-#                     G = Ar
-#                     bits_G = bits
+#                     G = Ar_now
+#                     bits_G, dele_G, deldc_G, thr_G, thrdc_G = bits, dele, deldc, thr, thrdc
 #                 else:
-#                     B = Ar
-#                     bits_B = bits
+#                     B = Ar_now
+#                     bits_B, dele_B, deldc_B, thr_B, thrdc_B = bits, dele, deldc, thr, thrdc
 #
-#         psnr_val[0,num] = bestPSNR
-#         bpp_val[0,num] = bpp_val_each
-#     bits = bits_R+bits_G+bits_B
+#     psnr_val = bestPSNR
+#     bpp_val = bpp_val_each
+#     bits = bits_R + bits_G + bits_B
 #     RGB = np.stack((R,G,B),2)
 #
 #     res_Ar = np.uint8(im_128 - RGB)
@@ -200,8 +199,99 @@ def test_Dict(filename, Bitrate_JPEG, Ds):
 #     res_bits = len(res_encoded) * 8
 #     print(res_bits+bits)
 #
+#     dele_fin = [dele_R, dele_G, dele_B]
+#     deldc_fin = [deldc_R, deldc_G, deldc_B]
+#     thr_fin = [thr_R,thr_G, thr_B]
+#     thrdc_fin = [thrdc_R,thrdc_G,thrdc_B]
 #
-#     img_Ar = Image.fromarray(np.uint8(RGB+res_Ar+128))
-#     img_Ar.save('test_noloss.png')
-#     return psnr_val, bpp_val
+#     # img_Ar = Image.fromarray(np.uint8(RGB+res_Ar+128))
+#     # img_Ar.save('test_noloss.png')
+#     return psnr_val, bpp_val,res_Ar ,xC, dele_fin, deldc_fin, thr_fin, thrdc_fin, xc_encoded, res_encoded
 
+
+# maybe used in lossless compression
+# def test_Dict(filename, Bitrate_JPEG, Ds):
+#     img = Image.open(filename).convert('RGB')
+#     img = np.array(img,dtype=np.double)
+#     img_hgt, img_wdt, img_ch = img.shape
+#     numBits = Bitrate_JPEG.shape[1]
+#     psnr_val = np.zeros((1, numBits))
+#     bpp_val = np.zeros((1,numBits))
+#     cur_QP = 60
+#     im_128 = img - 128
+#
+#     # for num in range(numBits):
+#     #     # print(num)
+#     #     if num > 8:
+#     #         break
+#
+#     bestQP = cur_QP
+#     target_bpp = 0.2
+#     par = test_Dict_par(dictionary = Ds, targetPSNR = 100 - cur_QP, dele = -18, qLimit = [0.4, 1.00], estimateBits = 'Huff06', ompMethod = 'mexOMP', verbose = 0)
+#
+#     img_in = np.concatenate((im_128[:,:,0],im_128[:,:,1],im_128[:,:,2]),axis=0)
+#     Ar, PSNR, xC, dele, deldc, thr, thrdc = imageapprox(img_in, par)
+#
+#     xc_encoded = bz2.compress((str(xC)).encode())
+#     bits = len(xc_encoded)*8
+#
+#     bestPSNR = PSNR
+#     bpp_val_each = bits/(img_hgt*img_wdt)
+#     cur_bpp = bpp_val_each
+#
+#     if cur_bpp > target_bpp:
+#         dir1 = -1
+#     else:
+#         dir1 = 1
+#
+#     dirchange = 0
+#     while bestPSNR<40:
+#         if cur_bpp > target_bpp:
+#             cur_QP = cur_QP + 1
+#             if dir1==-1:
+#                 dirchange = 0
+#             else:
+#                 dirchange = 1
+#
+#         else:
+#             cur_QP = cur_QP - 1
+#             if dir1==1:
+#                 dirchange = 0
+#             else:
+#                 dirchange = 1
+#
+#         if dirchange==1 or cur_QP>99 or cur_QP<1:
+#             break
+#
+#         par = test_Dict_par(dictionary=Ds, targetPSNR=100 - cur_QP, dele=-18, qLimit=[0.4, 1.00],
+#                             estimateBits='Huff06', ompMethod='mexOMP', verbose=0)
+#
+#         Ar_now, PSNR_now, xC_now, dele, deldc, thr, thrdc = imageapprox(img_in, par)
+#
+#         if bestPSNR>PSNR_now:
+#             continue
+#         else:
+#             Ar = Ar_now
+#             xC = xC_now
+#             xc_encoded = bz2.compress((str(xC)).encode())
+#             bits = len(xc_encoded) * 8
+#             bestPSNR = PSNR_now
+#             bpp_val_each = (bits) / (img_hgt * img_wdt)
+#             cur_bpp = bpp_val_each
+#
+#     dele_fin, deldc_fin, thr_fin, thrdc_fin = dele, deldc, thr, thrdc
+#     psnr_val = bestPSNR
+#     bpp_val = bpp_val_each
+#
+#     R = Ar[0:256,:]
+#     G = Ar[256:512,:]
+#     B = Ar[512:,:]
+#
+#     RGB = np.stack((R,G,B),2)
+#
+#     res_Ar = np.uint8(im_128 - RGB)
+#     res_encoded = bz2.compress((str(res_Ar)).encode())
+#
+#     img_Ar = Image.fromarray(np.uint8(RGB+128))
+#     img_Ar.save('test_RGB.png')
+#     return psnr_val, bpp_val,res_Ar ,xC, dele_fin, deldc_fin, thr_fin, thrdc_fin, xc_encoded, res_encoded
