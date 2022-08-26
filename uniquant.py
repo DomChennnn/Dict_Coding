c7f9585d3fabe15f40b6890ebc5851398bd631f3
Zenglu Liu <348133420@qq.com>
2022-08-26 17:26:50 +0800

    format

--- uniquant.py
+++ uniquant.py
@@ -1,10 +1,9 @@
 import numpy as np
 
 
+def uniquant(X, dele, thr, ymax=None):
+    """
 
-def uniquant(X, dele, thr, ymax = None):
-    '''
-    
     uniquant    Uniform scalar quantizer (or inverse quantizer) with threshold
     Note: Use three arguments for inverse quantizing and
        four arguments for quantizing.
@@ -21,21 +20,22 @@ def uniquant(X, dele, thr, ymax = None):
     :return Y    - the indexes for the quantizer cells, the bins are indexed as
       ..., -3, -2, -1, 0, 1, 2, 3, ...  where 0 is for the zero bin
     ----------------------------------------------
-    '''
+    """
     S = np.sign(X)
     X = abs(X)
 
     if ymax != None:
-        Y = np.floor((X - thr)/dele) + 1
+        Y = np.floor((X - thr) / dele) + 1
         if thr > dele:
-            Y[Y<0] = 0
+            Y[Y < 0] = 0
         ymax = np.floor(ymax)
-        Y[Y>ymax] = ymax
+        Y[Y > ymax] = ymax
     elif ymax == None:
         Y = np.zeros_like(X)
         I = np.nonzero(X)
-        Y[I] = X[I] * dele + (thr - dele/2)
+        Y[I] = X[I] * dele + (thr - dele / 2)
     else:
-        print('uniquant error')
+        print("uniquant error")
     Y = Y * S
+
     return Y
