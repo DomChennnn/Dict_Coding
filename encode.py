2a26df4742984a287aec9f3c93a0255123604333
Zenglu Liu <348133420@qq.com>
2022-09-01 20:13:54 +0800

    fix

--- encode.py
+++ encode.py
@@ -48,9 +48,9 @@ def img_encode(path_in, path_out, Ds, mode=0):
         info1 = [len(xc_encoded_R), len(xc_encoded_G), len(xc_encoded_B)]
         info2 = [deldc_R, deldc_G, deldc_B]
         saved = open(path_out, mode="wb")
-        bytes_cnt = write_uints(saved, (info0[0], info0[1]))
-        bytes_cnt = write_uints(saved, (info1[0], info1[1], info1[2]))
-        bytes_cnt = write_uints(saved, (info2[0], info2[1], info2[2]))
+        write_uints(saved, (info0[0], info0[1]))
+        write_uints(saved, (info1[0], info1[1], info1[2]))
+        write_uints(saved, (info2[0], info2[1], info2[2]))
         saved.write(mybits)
         saved.close()
     elif mode == 1:
@@ -104,11 +104,11 @@ def img_encode(path_in, path_out, Ds, mode=0):
         ]
         info2 = [deldc_R, deldc_G, deldc_B]
         saved = open(path_out, mode="wb")
-        bytes_cnt = write_uints(saved, (info0[0], info0[1]))
-        bytes_cnt = write_uints(
+        write_uints(saved, (info0[0], info0[1]))
+        write_uints(
             saved, (info1[0], info1[1], info1[2], info1[3], info1[4], info1[5])
         )
-        bytes_cnt = write_uints(saved, (info2[0], info2[1], info2[2]))
+        write_uints(saved, (info2[0], info2[1], info2[2]))
         saved.write(mybits)
         saved.close()
     else:
